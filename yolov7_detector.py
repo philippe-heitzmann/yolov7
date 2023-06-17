4ca0328beb5eef57ddd3124cf50ddaa948addf6b
Philippe Heitzmann <phil062497@gmail.com>
2023-06-17 11:21:46 -0400

    updated weights loading

--- yolov7_detector.py
+++ yolov7_detector.py
@@ -12,7 +12,7 @@ from utils.general import check_img_size, non_max_suppression, apply_classifier
 from utils.torch_utils import TracedModel, load_classifier, select_device
 
 class YOLOv7Detector:
-    def __init__(self, weights='/weights/yolov7.pt', source='inference/images', img_size=640, device='', half=True,
+    def __init__(self, weights=['/weights/yolov7.pt'], source='inference/images', img_size=640, device='', half=True,
                  trace=True, augment=True, conf_thres=0.25, iou_thres=0.45, classes=None, agnostic_nms=False, classifier=False):
         self.weights = weights
         self.source = source
